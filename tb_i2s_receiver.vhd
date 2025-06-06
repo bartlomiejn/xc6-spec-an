40723d1a0ddc391927a488a307a9c5e507b89b46
Bartlomiej Nowak <bartlomiej.nwk@gmail.com>
2025-06-06 21:57:49 +0200

    Fix identation

--- tb_i2s_receiver.vhd
+++ tb_i2s_receiver.vhd
@@ -2,12 +2,12 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 library std;
 use std.textio.all;
- 
+
 ENTITY tb_i2s_receiver IS
 END tb_i2s_receiver;
- 
-ARCHITECTURE behavior OF tb_i2s_receiver IS 
- 
+
+ARCHITECTURE behavior OF tb_i2s_receiver IS
+
     COMPONENT i2s_receiver
     PORT(
          I2S_I_RESET : IN  std_logic;
@@ -22,38 +22,38 @@ ARCHITECTURE behavior OF tb_i2s_receiver IS
         );
     END COMPONENT;
 
-   --Inputs
-   signal I2S_I_RESET : std_logic := '0';
-   signal I2S_I_SYSCLK : std_logic := '0';
-   signal I2S_I_DATA : std_logic := '0';
-   signal I2S_I_TREADY : std_logic := '0';
+    --Inputs
+    signal I2S_I_RESET : std_logic := '0';
+    signal I2S_I_SYSCLK : std_logic := '0';
+    signal I2S_I_DATA : std_logic := '0';
+    signal I2S_I_TREADY : std_logic := '0';
 
- 	--Outputs
-   signal I2S_O_MCLK : std_logic;
-   signal I2S_O_LRCLK : std_logic;
-   signal I2S_O_BCLK : std_logic;
-   signal I2S_O_TDATA : std_logic_vector(31 downto 0);
-   signal I2S_O_TVALID : std_logic;
-	
+    --Outputs
+    signal I2S_O_MCLK : std_logic;
+    signal I2S_O_LRCLK : std_logic;
+    signal I2S_O_BCLK : std_logic;
+    signal I2S_O_TDATA : std_logic_vector(31 downto 0);
+    signal I2S_O_TVALID : std_logic;
+
 	-- Stimulus signals
 	signal I2S_O_MCLK_count : integer := 0;
 	signal I2S_O_LRCLK_count : integer := 0;
 	signal I2S_O_BCLK_count : integer := 0;
 
-	signal i2s_mock_left : std_logic_vector(31 downto 0) 
-		:= "11000011010110100000111100011000";
-	signal i2s_mock_right : std_logic_vector(31 downto 0) 
+	signal i2s_mock_left : std_logic_vector(31 downto 0)
+	    := "11000011010110100000111100011000";
+	signal i2s_mock_right : std_logic_vector(31 downto 0)
 		:= "00111100101001011111000011100111";
 
-   -- Stimulus periods
-   constant I2S_I_SYSCLK_period : time := 40.6901 ns; -- 24.576MHz
+    -- Stimulus periods
+    constant I2S_I_SYSCLK_period : time := 40.6901 ns; -- 24.576MHz
 	constant I2S_I_RESET_period : time := 100 ns;
 	constant test_period : time := I2S_I_SYSCLK_period * 1024;
 BEGIN
- 
-   uut: i2s_receiver 
-		port map (
-			I2S_I_RESET => I2S_I_RESET,
+
+    uut: i2s_receiver
+	    port map (
+		    I2S_I_RESET => I2S_I_RESET,
 			I2S_I_SYSCLK => I2S_I_SYSCLK,
 			I2S_I_DATA => I2S_I_DATA,
 			I2S_I_TREADY => I2S_I_TREADY,
@@ -82,7 +82,7 @@ BEGIN
 				I2S_I_DATA <= i2s_mock_left(i);
 				wait until falling_edge(I2S_O_MCLK);
 			end loop;
-		
+
 			-- Right channel mock data
 			wait until falling_edge(I2S_O_LRCLK);
 			for i in i2s_mock_right'range loop
@@ -93,11 +93,11 @@ BEGIN
 	end process;
 
    stimulus_main: process
-   begin		
+   begin
 		I2S_I_RESET <= '1';
 		wait for I2S_I_RESET_period;
 		I2S_I_RESET <= '0';
-		
+
 		assert I2S_O_MCLK_count = 0 and I2S_O_LRCLK_count = 0 and I2S_O_BCLK_count = 0
 			report "Clocks should not pulse when reset is asserted"
 			severity error;
@@ -107,10 +107,10 @@ BEGIN
 		assert I2S_O_BCLK_count = 128 and I2S_O_LRCLK_count = 2 and I2S_O_MCLK_count = 1
 			report "Output clocks did not pulse expected number of times"
 			severity error;
-			
+
 		wait;
    end process;
-	
+
 	-- Output clock counters
 	count_ticks_I2S_O_MCLK: process(I2S_O_MCLK)
 	begin
@@ -118,27 +118,27 @@ BEGIN
 			I2S_O_MCLK_count <= I2S_O_MCLK_count + 1;
 		end if;
 	end process;
-	
+
 	count_ticks_I2S_O_LRCLK: process(I2S_O_LRCLK)
 	begin
 		if rising_edge(I2S_O_LRCLK) then
 			I2S_O_LRCLK_count <= I2S_O_LRCLK_count + 1;
 		end if;
 	end process;
-	
+
 	count_ticks_I2S_O_BCLK: process(I2S_O_BCLK)
 	begin
 		if rising_edge(I2S_O_BCLK) then
 			I2S_O_BCLK_count <= I2S_O_BCLK_count + 1;
 		end if;
 	end process;
-	
+
 	-- Generate report
 	report_results: process
 		variable L: line;
 	begin
 		wait for test_period + I2S_I_RESET_period;
-		
+
 		write(L, string'("I2S_O_MCLK ticks: "));
 		write(L, I2S_O_MCLK_count);
 		writeline(output, L);
@@ -155,3 +155,5 @@ BEGIN
 	end process;
 
 END;
+
+
